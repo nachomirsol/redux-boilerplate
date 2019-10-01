25ca05d8de7953375cebca9c16779ccfb21c759a
JUAN IGNACIO MIRALLES SOLER <jimiralles@aguasdevalencia.es>
2019-10-01 15:47:39 +0200

    undo changes

--- src/containers/TextPage/index.js
+++ src/containers/TextPage/index.js
@@ -1,18 +1,19 @@
 
 import React from 'react';
-
+import Layout from '../../components/Layout';
 import { injectIntl } from 'react-intl';
 import './TextPage.scss';
 
-// This case we use the layout in the route instead of the component directly
 const TextPage = ({ intl }) => {
     return (
-        <div className="row">
-            <div className="col-md-6">
-                <h4>Texts</h4>
-                <p>{intl.formatMessage({ id: 'TEXT' })}</p>
+        <Layout>
+            <div className="row">
+                <div className="col-md-6">
+                    <h4>Texts</h4>
+                    <p>{intl.formatMessage({ id: 'TEXT' })}</p>
+                </div>
             </div>
-        </div>
+        </Layout>
     )
 }
 export default injectIntl(TextPage)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,6 @@ import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 import { URL } from '../utils/const';
 import HomePage from '../containers/HomePage';
-import Layout from '../../components/Layout';
 import PostPage from '../containers/PostPage';
 import TextPage from '../containers/TextPage';
 
@@ -11,8 +10,6 @@ export const Routes = () => (
     <Switch>
         <Route exact={true} path="/" component={HomePage} />
         <Route exact={true} path={URL.posts} component={PostPage} />
-        <Layout>
-            <Route exact={true} path={URL.texts} component={TextPage} />
-        </Layout>
+        <Route exact={true} path={URL.texts} component={TextPage} />
     </Switch>
 );
