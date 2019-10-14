e31bdbd4a8c0bdc2ff788a8a9cd6d27ee8446bc2
JUAN IGNACIO MIRALLES SOLER <jimiralles@aguasdevalencia.es>
2019-10-14 12:07:56 +0200

    play with css grid

--- src/containers/TextPage/index.js
+++ src/containers/TextPage/index.js
@@ -6,9 +6,29 @@ import './TextPage.scss';
 // This case we use the layout in the route instead of the component directly
 const TextPage = ({ intl }) => {
     return (
-        <div className="row">
-            <div className="col-md-6">
-                <h4>Texts</h4>
+        <div className="grid-container">
+            <div className="element">
+                <h4>Text 1</h4>
+                <p>{intl.formatMessage({ id: 'TEXT' })}</p>
+            </div>
+            <div className="element">
+                <h4>Text 2</h4>
+                <p>{intl.formatMessage({ id: 'TEXT' })}</p>
+            </div>
+            <div className="element">
+                <h4>Text 3</h4>
+                <p>{intl.formatMessage({ id: 'TEXT' })}</p>
+            </div>
+            <div className="element">
+                <h4>Text 4</h4>
+                <p>{intl.formatMessage({ id: 'TEXT' })}</p>
+            </div>
+            <div className="element">
+                <h4>Text 5</h4>
+                <p>{intl.formatMessage({ id: 'TEXT' })}</p>
+            </div>
+            <div className="element">
+                <h4>Text 6</h4>
                 <p>{intl.formatMessage({ id: 'TEXT' })}</p>
             </div>
         </div>
