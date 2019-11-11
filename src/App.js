75bb5320ffaf319e6df6d76134f82c1b93ef9bf6
JUAN IGNACIO MIRALLES SOLER <jimiralles@aguasdevalencia.es>
2019-11-11 11:54:39 +0100

    add themification with sass

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import './App.scss';
+import "./styles/main.scss";
 
 const routes = Routes();
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter children={routes} basename={"/"} />
-    </Provider>
+    <div className="theme-light">
+      <Provider store={store}>
+        <BrowserRouter children={routes} basename={"/"} />
+      </Provider>
+    </div>
+
   )
 }
 
